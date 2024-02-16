4b7cba3df89fb268263240cb74373e18f53b0863
nrodichenko <nsr@tsuru.su>
2024-02-16 17:36:47 +0300

    Update generator script to choose wireshark plugin directory + cleanup

--- kaiken_generate.sh
+++ kaiken_generate.sh
@@ -1,27 +1,39 @@
-#!/bin/bash
+#!/bin/sh
 
 # Generate html for Kaiken dialect
-cd doc && python3 mavlink_gitbook.py
+cd doc
+python mavlink_gitbook.py
 mkdir -p publish
 cp messages/_html/kaiken.html publish
-cd ../
+cd ..
 
 # Generate C-lib
-python3 -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output=generated/mavlink_kaiken_v2 message_definitions/v1.0/kaiken.xml
+python -m pymavlink.tools.mavgen --lang=C --wire-protocol=2.0 --output=generated/mavlink_kaiken_v2 message_definitions/v1.0/kaiken.xml
 # Overwrite message ID in generated C-lib
-python3 ./kaiken_clib_overwrite.py -d generated/mavlink_kaiken_v2/kaiken
+python ./kaiken_clib_overwrite.py -d generated/mavlink_kaiken_v2/kaiken
+
 # Generate Python libs and reinstall pymavlink
-pip uninstall pymavlink
+pip uninstall -y pymavlink
 cd pymavlink
 python setup.py install
-cd ../
+cd ..
+
 # Overwrite message ID in generated Python-lib
 pymavlink_path=$(pip show pymavlink | sed -n 's/.*Location://p')/pymavlink
-python3 ./kaiken_pylib_overwrite.py -d ${pymavlink_path}/dialects/v20
+python ./kaiken_pylib_overwrite.py -d ${pymavlink_path}/dialects/v20
+
 # Fix mavutil.py
-python3 ./kaiken_mavutil_overwrite.py -d ${pymavlink_path}
+python ./kaiken_mavutil_overwrite.py -d ${pymavlink_path}
+
 # Generate lib for Wireshark
-python3 -m pymavlink.tools.mavgen --lang=WLua --wire-protocol=2.0 --output=generated/mavlink_kaiken_v2 message_definitions/v1.0/kaiken.xml
+python -m pymavlink.tools.mavgen --lang=WLua --wire-protocol=2.0 --output=generated/mavlink_kaiken_v2 message_definitions/v1.0/kaiken.xml
+
 # Copy Lua script
-echo Copy mavlink_kaiken_v2.lua to Wireshark plugin folder
-sudo cp generated/mavlink_kaiken_v2.lua /usr/lib/x86_64-linux-gnu/wireshark/plugins/mavlink_kaiken_v2.lua
+if [ -z ${WIRESHARK_PLUGIN_FOLDER+x} ]
+then
+	echo Copying mavlink_kaiken_v2.lua to Wireshark plugin folder $WIRESHARK_PLUGIN_FOLDER
+	sudo /bin/cp -rf generated/mavlink_kaiken_v2.lua $WIRESHARK_PLUGIN_FOLDER/mavlink_kaiken_v2.lua
+else
+	echo Copying mavlink_kaiken_v2.lua to Wireshark plugin folder /usr/lib/x86_64-linux-gnu/wireshark/plugins
+	sudo cp generated/mavlink_kaiken_v2.lua /usr/lib/x86_64-linux-gnu/wireshark/plugins/mavlink_kaiken_v2.lua
+fi
